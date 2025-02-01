de9e5386ea939ac6cb2ed63817a91b05ea19a4b8
julien <julienyang12@gmail.com>
2025-02-01 15:06:55 -0500

    new routes for coords and paths

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
  # CREATE READ UPDATE DELETE - CRUD
 from flask import request, jsonify
 from config import app, db
-from models import User
+from models import User, Coordinates, Path
 
+## USER LIST
 # READ
 @app.route("/users", methods=['GET'])
 def get_users():
@@ -10,7 +11,6 @@ def get_users():
     return jsonify({"users": [user.to_json() for user in users]})
 
 # CREATE
-
 @app.route("/create_user", methods=['POST'])
 def create_user():
     first_name = request.json.get("firstName")
@@ -30,7 +30,6 @@ def create_user():
     return jsonify({"message": "User created!"}), 201
 
 # UPDATE
-
 @app.route("/update_user/<int:user_id>", methods=['PATCH'])
 def update_user(user_id):
     user = User.query.get(user_id)
@@ -47,7 +46,6 @@ def update_user(user_id):
     return jsonify({"message": "User updated"}), 200
 
 # DELETE
-
 @app.route("/delete_user/<int:user_id>", methods=["DELETE"])
 def delete_user(user_id):
     user = User.query.get(user_id)
@@ -60,6 +58,73 @@ def delete_user(user_id):
 
     return jsonify({"message": "User deleted"}), 200
 
+## COORDINATES
+# READ Coordinates
+@app.route("/coords", methods=['GET'])
+def get_coords():
+    coords = Coordinates.query.all()
+    return jsonify({"coords": [coord.to_json() for coord in coords]})
+
+#Create coord object
+@app.route("/create_coord", methods=['GET'])
+def create_coord():    
+    longitude_1 = request.json.get("longitude_1")
+    latitude_1 = request.json.get("latitude_1")
+    longitude_2 = request.json.get("longitude_2")
+    latitude_2 = request.json.get("latitude_2")
+
+    if not longitude_1 or not latitude_1 or not longitude_2 or not latitude_2:
+        return (jsonify({"message": "Invalid data"}), 400,)
+    
+    new_coord = Coordinates(longitude_1=longitude_1, latitude_1=latitude_1, longitude_2=longitude_2, latitude_2=latitude_2)
+
+    try:
+        db.session.add(new_coord)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+    return jsonify({"message": "Coord created!"}), 201
+
+### PATHS
+#READ Recent Path
+@app.route("/paths", methods=['GET'])
+def get_paths():
+    paths = Path.query.first()
+    return jsonify({"paths": [path.to_json() for path in paths]})
+
+#Create path
+@app.route("/create_path/<int:coord_id>", methods=['POST'])
+def create_path(coord_id):
+    coord = Coordinates.query.get(coord_id)
+    if not coord:
+        return jsonify({"message": "Coord not found"}), 404
+    
+    extra = request.args.get("extra")
+    if extra:
+        user = User.query.get(extra)
+    if user:
+        weight = user.weight
+        height = user.height
+    else:
+        weight = 70
+        height = 170
+
+    if not longitude_1 or not latitude_1 or not longitude_2 or not latitude_2:
+        return (jsonify({"message": "Missing Coords"}), 400,)
+    
+    coord_tuple = ((coord.longitude_1, coord.latitude_1), (coord.longitude_2, coord.latitude_2), (weight, height))
+    #GET PATH WITH COOL FUNCTION
+    new_path = "HTML COOL PATH"
+    
+    try:
+        db.session.add(new_path)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "Path created!!"}), 201
+    
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()

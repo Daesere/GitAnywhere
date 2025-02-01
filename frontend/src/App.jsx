88ab9c6777ec39048e83cb44ed50810b5ce6ae71
julien <julienyang12@gmail.com>
2025-02-01 12:11:04 -0500

    working user interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,55 @@
 import { useState, useEffect } from 'react'
+import UserList from './UserList'
 import './App.css'
+import UserForm from './UserForm'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentUser, setCurrentUser] = useState([])
+  useEffect(() => {
+    fetchUsers()
+  }, [])
 
+  const fetchUsers = async () => {
+    const response = await fetch('http://127.0.0.1:5000/users');
+    const data = await response.json();
+    setUsers(data.users);
+  }
 
-  return (
-    <>
-      <h1>Counter</h1> 
-    </>
-  )
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentUser({})
+  }
+
+  const openCreateModal = () => { 
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+  
+  const openEditModal = (user) => {
+    if (isModalOpen) return 
+    setCurrentUser(user)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+    fetchUsers()
+  }
+  return <>
+    <UserList users={users} updateUser={openEditModal} updateCallback={ onUpdate } />
+    <button onClick={openCreateModal}>Create New User</button>
+    {isModalOpen && <div className="modal">
+      <div classname="modal-content">
+        <span classname="close" onClick={closeModal}>&times;</span>
+        <UserForm existingUser={currentUser} updateCallback={ onUpdate } />
+        </div>
+    </div>
+    }
+    
+  </>
+    
+  
 }
 
 export default App
---
+++ frontend/src/UserForm.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+
+const UserForm = ({ existingUser = {}, updateCallback }) => {
+    const [firstName, setFirstName] = useState(existingUser.firstName || "");
+    const [weight, setWeight] = useState(existingUser.weight || "");
+    const [height, setHeight] = useState(existingUser.height || "");
+
+    const updating = Object.entries(existingUser).length !== 0;
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        const data = {
+            firstName,
+            weight,
+            height
+        }
+        const url = "http://127.0.0.1:5000/" + ( updating  ? `update_user/${existingUser.id}` : "create_user")
+        const options = {
+            method: updating ? "PATCH" : "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status !== 200) {
+            const data = await response.json()
+            alert(data.message)
+        } else {
+            updateCallback()
+        }
+    }
+    return <form onSubmit={onSubmit}>
+        <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+                type="text"
+                id="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="weight">Weight:</label>
+            <input
+                type="text"
+                id="weight"
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="height">Height:</label>
+            <input
+                type="text"
+                id="height"
+                value={height}
+                onChange={(e) => setHeight(e.target.value)}
+            />
+        </div>
+        <button type="submit">{updating ? "Update" : "Create"}</button>
+    </form>
+}
+
+export default UserForm
